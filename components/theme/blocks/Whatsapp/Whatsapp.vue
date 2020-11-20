109d17385aa9f44dedbf69e95430a8d40f371851
egor.pisarev <egor.pisarev@gmail.com>
2020-11-20 09:05:10 +0400

    WhatsApp link added

---
+++ components/theme/blocks/Whatsapp/Whatsapp.vue
@@ -0,0 +1,28 @@
+<template>
+  <a href="https://wa.me/79133952306" class="whatsapp-link" target="_blank" title="WhatsApp">
+    <img src="/assets/whatsapp-logo.png" />
+  </a>
+</template>
+<style scoped lang="scss">
+.whatsapp-link {
+  display: block;
+  position: fixed;
+  left: 10px;
+  bottom: 10px;
+  z-index: 100;
+  img {
+    width: 78px;
+    transition: width 0.3s, height 0.3s;
+  }
+  &:hover {
+    text-decoration: none;
+    img {
+      width: 95px;
+    }
+    &:after {
+      background: none;
+      height: 0px;
+    }
+  }
+}
+</style>
--- layouts/Default.vue
+++ layouts/Default.vue
@@ -4,27 +4,10 @@
     <loader />
     <div id="viewport" class="w-100 relative">
       <main-header />
-      <async-sidebar
-        :async-component="SearchPanel"
-        :is-open="isSearchPanelOpen"
-        @close="$store.commit('ui/setSearchpanel')"
-      />
-      <async-sidebar
-        :async-component="Microcart"
-        :is-open="isMicrocartOpen"
-        @close="$store.commit('ui/setMicrocart')"
-      />
-      <async-sidebar
-        :async-component="SidebarMenu"
-        :is-open="isSidebarOpen"
-        @close="$store.commit('ui/setSidebar')"
-        direction="left"
-      />
-      <async-sidebar
-        :async-component="Wishlist"
-        :is-open="isWishlistOpen"
-        @close="$store.commit('ui/setWishlist')"
-      />
+      <async-sidebar :async-component="SearchPanel" :is-open="isSearchPanelOpen" @close="$store.commit('ui/setSearchpanel')" />
+      <async-sidebar :async-component="Microcart" :is-open="isMicrocartOpen" @close="$store.commit('ui/setMicrocart')" />
+      <async-sidebar :async-component="SidebarMenu" :is-open="isSidebarOpen" @close="$store.commit('ui/setSidebar')" direction="left" />
+      <async-sidebar :async-component="Wishlist" :is-open="isWishlistOpen" @close="$store.commit('ui/setWishlist')" />
       <slot />
       <main-footer />
       <notification />
@@ -32,84 +15,85 @@
       <cookie-notification />
       <offline-badge />
       <order-confirmation :orders-data="ordersData" v-if="loadOrderConfirmation" />
+      <whatsapp />
     </div>
     <vue-progress-bar />
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import AsyncSidebar from 'theme/components/theme/blocks/AsyncSidebar/AsyncSidebar.vue'
-import MainHeader from 'theme/components/core/blocks/Header/Header.vue'
-import MainFooter from 'theme/components/core/blocks/Footer/Footer.vue'
-import Overlay from 'theme/components/core/Overlay.vue'
-import Loader from 'theme/components/core/Loader.vue'
-import Notification from 'theme/components/core/Notification.vue'
-import SignUp from 'theme/components/core/blocks/Auth/SignUp.vue'
-import CookieNotification from 'theme/components/core/CookieNotification.vue'
-import OfflineBadge from 'theme/components/core/OfflineBadge.vue'
-import { isServer } from '@vue-storefront/core/helpers'
-import Head from 'theme/head'
-import config from 'config'
+import { mapState } from "vuex";
+import AsyncSidebar from "theme/components/theme/blocks/AsyncSidebar/AsyncSidebar.vue";
+import MainHeader from "theme/components/core/blocks/Header/Header.vue";
+import MainFooter from "theme/components/core/blocks/Footer/Footer.vue";
+import Overlay from "theme/components/core/Overlay.vue";
+import Loader from "theme/components/core/Loader.vue";
+import Notification from "theme/components/core/Notification.vue";
+import SignUp from "theme/components/core/blocks/Auth/SignUp.vue";
+import CookieNotification from "theme/components/core/CookieNotification.vue";
+import OfflineBadge from "theme/components/core/OfflineBadge.vue";
+import Whatsapp from "theme/components/theme/blocks/Whatsapp/Whatsapp.vue";
 
-const SidebarMenu = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-sidebar-menu" */ 'theme/components/core/blocks/SidebarMenu/SidebarMenu.vue')
-const Microcart = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-microcart" */ 'theme/components/core/blocks/Microcart/Microcart.vue')
-const Wishlist = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-wishlist" */ 'theme/components/core/blocks/Wishlist/Wishlist.vue')
-const SearchPanel = () => import(/* webpackChunkName: "vsf-search-panel" */ 'theme/components/core/blocks/SearchPanel/SearchPanel.vue')
-const OrderConfirmation = () => import(/* webpackChunkName: "vsf-order-confirmation" */ 'theme/components/core/blocks/Checkout/OrderConfirmation.vue')
+import { isServer } from "@vue-storefront/core/helpers";
+import Head from "theme/head";
+import config from "config";
+
+const SidebarMenu = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-sidebar-menu" */ "theme/components/core/blocks/SidebarMenu/SidebarMenu.vue");
+const Microcart = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-microcart" */ "theme/components/core/blocks/Microcart/Microcart.vue");
+const Wishlist = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-wishlist" */ "theme/components/core/blocks/Wishlist/Wishlist.vue");
+const SearchPanel = () => import(/* webpackChunkName: "vsf-search-panel" */ "theme/components/core/blocks/SearchPanel/SearchPanel.vue");
+const OrderConfirmation = () => import(/* webpackChunkName: "vsf-order-confirmation" */ "theme/components/core/blocks/Checkout/OrderConfirmation.vue");
 
 export default {
-  data () {
+  data() {
     return {
       loadOrderConfirmation: false,
       ordersData: [],
       Microcart,
       Wishlist,
       SearchPanel,
-      SidebarMenu
-    }
+      SidebarMenu,
+    };
   },
   computed: {
     ...mapState({
-      overlayActive: state => state.ui.overlay,
-      isSearchPanelOpen: state => state.ui.searchpanel,
-      isSidebarOpen: state => state.ui.sidebar,
-      isMicrocartOpen: state => state.ui.microcart,
-      isWishlistOpen: state => state.ui.wishlist
-    })
+      overlayActive: (state) => state.ui.overlay,
+      isSearchPanelOpen: (state) => state.ui.searchpanel,
+      isSidebarOpen: (state) => state.ui.sidebar,
+      isMicrocartOpen: (state) => state.ui.microcart,
+      isWishlistOpen: (state) => state.ui.wishlist,
+    }),
   },
   methods: {
-    onOrderConfirmation (payload) {
-      this.loadOrderConfirmation = true
-      this.ordersData = payload
-      this.$bus.$emit('modal-show', 'modal-order-confirmation')
+    onOrderConfirmation(payload) {
+      this.loadOrderConfirmation = true;
+      this.ordersData = payload;
+      this.$bus.$emit("modal-show", "modal-order-confirmation");
     },
-    fetchMenuData () {
-      return this.$store.dispatch('category-next/fetchMenuCategories', {
-        level: config.entities.category.categoriesDynamicPrefetch && config.entities.category.categoriesDynamicPrefetchLevel >= 0
-          ? config.entities.category.categoriesDynamicPrefetchLevel
-          : null,
-        skipCache: isServer
-      })
-    }
+    fetchMenuData() {
+      return this.$store.dispatch("category-next/fetchMenuCategories", {
+        level: config.entities.category.categoriesDynamicPrefetch && config.entities.category.categoriesDynamicPrefetchLevel >= 0 ? config.entities.category.categoriesDynamicPrefetchLevel : null,
+        skipCache: isServer,
+      });
+    },
   },
-  serverPrefetch () {
-    return this.fetchMenuData()
+  serverPrefetch() {
+    return this.fetchMenuData();
   },
-  beforeMount () {
+  beforeMount() {
     // Progress bar on top of the page
     this.$router.beforeEach((to, from, next) => {
-      this.$Progress.start()
-      this.$Progress.increase(40)
-      next()
-    })
+      this.$Progress.start();
+      this.$Progress.increase(40);
+      next();
+    });
     this.$router.afterEach((to, from) => {
-      this.$Progress.finish()
-    })
-    this.$bus.$on('offline-order-confirmation', this.onOrderConfirmation)
+      this.$Progress.finish();
+    });
+    this.$bus.$on("offline-order-confirmation", this.onOrderConfirmation);
   },
-  beforeDestroy () {
-    this.$bus.$off('offline-order-confirmation', this.onOrderConfirmation)
+  beforeDestroy() {
+    this.$bus.$off("offline-order-confirmation", this.onOrderConfirmation);
   },
   metaInfo: Head,
   components: {
@@ -123,9 +107,10 @@ export default {
     CookieNotification,
     OfflineBadge,
     OrderConfirmation,
-    AsyncSidebar
-  }
-}
+    AsyncSidebar,
+    Whatsapp,
+  },
+};
 </script>
 
 <style lang="scss" src="theme/css/main.scss"></style>
